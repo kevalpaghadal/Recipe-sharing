1283ec1a7eef1de6ce21ef5716af25e70306a45a
kevalpaghadal <myleptop.vue0m8eg@gmail.com>
2024-02-10 02:55:02 +0530

    create app account

---
+++ account/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from .models import User
+from django.contrib.auth.admin import UserAdmin
+# Register your models here.
+
+# admin site password nonedit and display which field are show
+class CustomUserAdmin(UserAdmin):
+    list_display = ('email' , 'first_name' , 'last_name' , 'username' , 'is_active')
+    ordering = ('-date_joined',) # - means descending order
+    # list_display_links = ('email' , 'username')
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+admin.site.register(User , CustomUserAdmin)
