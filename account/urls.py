fcf0ac79aef9f7c709965e0b25c15839662d7350
kevalpaghadal <myleptop.vue0m8eg@gmail.com>
2024-02-16 03:14:40 +0530

    login fuctionality complete.

--- account/urls.py
+++ account/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('loginUser/' , views.loginUser , name='loginUser'),
     path('registerUser/', views.registerUser, name='registerUser'),
+    path('logoutUser/' , views.logoutUser , name='logoutUser'),
     path('userProfile/' , views.userProfile, name='userProfile'),
     path('addRecipe/', views.addRecipe , name='addRecipe'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render , redirect
 from .forms import UserForm
-from django.contrib import messages
+from django.contrib import messages , auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-def loginUser(request):
-    return render(request , 'account/loginUser.html')
-
 def registerUser(request):
-    if request.method == 'POST':
+    if request.user.is_authenticated:
+        messages.warning(request , 'You are already logged in!')
+        return redirect('home')
+    elif request.method == 'POST':  
         # print(request.POST)
         form = UserForm(request.POST)
         if form.is_valid():
@@ -28,8 +29,34 @@ def registerUser(request):
     }
     return render(request , 'account/registerUser.html' , context)
 
+def loginUser(request):
+    if request.user.is_authenticated:
+        messages.warning(request , 'You are already logged in!')
+        return redirect('home')
+    elif request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        user = auth.authenticate(email=email , password=password)
+
+        if user is not None:
+            auth.login(request , user)
+            messages.success(request , 'You are now logged in. ')
+            return redirect('home')
+        else:
+            messages.error(request , 'Invalid login')
+            return redirect('loginUser')
+    return render(request , 'account/loginUser.html')
+
+def logoutUser(request):
+    auth.logout(request)
+    messages.info(request , 'You are logged out.')
+    return redirect('loginUser')
+
+@login_required(login_url = 'loginUser')
 def userProfile(request):
     return render(request , 'account/userProfile.html')
 
+@login_required(login_url = 'loginUser')
 def addRecipe(request):
     return render(request , 'account/addRecipe.html')
